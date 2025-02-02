15427720eef106316f2d96ec49e67a24b8486ade
Lucasfdp <lucasfdpinedo@gmail.com>
2025-02-02 21:28:52 +0100

    Add files via upload

---
+++ cursus/libft 2.02.25/ft_strchr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luferna3 <luferna3@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 14:56:30 by luferna3          #+#    #+#             */
+/*   Updated: 2025/02/02 21:19:40 by luferna3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	int		i;
+	char	a;
+
+	i = 0;
+	a = c;
+	while (str[i] != '\0')
+	{
+		if (str[i] == a)
+		{
+			return ((char *)(str + i));
+		}
+		i++;
+	}
+	if (a == '\0')
+	{
+		return ((char *)(str + i));
+	}
+	return (NULL);
+}
+
+// int	main(void)
+// {
+// 	char	str[] = "A new string";
+// 	char	c = '\0';
+
+// 	char	*result = strchr(str, c);
+// 	if (result != NULL)
+// 	{
+// 		printf("%c was found at the position %ld",c,  result - str);
+// 	}
+// 	else 
+// 	printf("%c was not found in the string", c);
+// 	return (0);
+// }
---
+++ cursus/libft 2.02.25/ft_strlen.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luferna3 <luferna3@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 15:50:56 by luferna3          #+#    #+#             */
+/*   Updated: 2025/02/02 21:22:26 by luferna3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+// int	main(void)
+// {
+// 	char	str[] = "General Kenobi";
+
+// 	int	result = ft_strlen(str);
+
+// 	printf("%d", result);
+// 	return (0);
+// }
